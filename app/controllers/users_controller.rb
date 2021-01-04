952f3769962323fff5b4e756afcdf31adaf498af
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2021-01-04 00:19:53 -0300

    first commit users controller and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(register_params)
+    if @user.save!
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def register_params
+    params.require(:user).permit(:email, :password)
+  end
+
+  def user_params
+    params.require(:user).permit(:user_name, :info, :favorite_genres, :photo)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#homepage'
+
+  scope 'users' do
+    get 'cadastro', to: 'users#new', as: 'new_user'
+    post 'cadastro', to: 'users#create'
+  end
+
 end
