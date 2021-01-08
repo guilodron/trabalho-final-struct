e02693ebbcb92b112de80a8b702560ea10345376
Gabriel Xavier Cardoso <gabrielxavier@pop-os.localdomain>
2021-01-07 21:13:51 -0300

    controller_movies

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,13 @@ Rails.application.routes.draw do
     post 'login', to: 'sessions#create'
     delete 'logout', to: 'sessions#destroy', as: :logout
   end
+
+  scope 'movies' do
+    get '/index', to: 'movies#index', as: :index_movies
+    get '/new', to: 'movies#new', as: :new_movie
+    post '/new', to: 'movies#create', as: :create_movie
+    get '/edit/:id', to: 'movies#edit', as: :edit_movie
+    get '/show/:id', to: 'movies#show', as: :show_movie
+    delete '/delete/:id', to: 'movies#destroy', as: :destroy_movie
+  end
 end
