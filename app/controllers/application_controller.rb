e873b474da3de1ca2c00dd494514c61f2dd7eb9c
Guilherme Rodrigues <guilodron7@gmail.com>
2021-01-15 23:08:53 -0300

    Quick save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   def homepage
+    @movies = Movie.order('created_at DESC').limit(5)
+    @best_rated = Movie.order('final_score DESC').limit(5)
     @todos = User.all
   end
 end
